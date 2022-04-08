b3b4afe71f3884d6fbecc49fa297774ea75faa72
Eze Anthony <anayo.uxui.fullstack@gmail.com>
2022-04-07 20:38:50 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main -Entry point
+ * My hello use printf task
+ * Return: return 0
+ */
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
