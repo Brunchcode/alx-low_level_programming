84c62be8755fcd2102573fa9b4ef43852c4cdbfe
Eze Anthony <anayo.uxui.fullstack@gmail.com>
2022-04-07 22:05:40 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry point
  *
- * Return: 0 after printing function
+ * Return: 0 after printing the function
  */
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
