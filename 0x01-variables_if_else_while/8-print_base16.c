6608b4b631e10b8ff1285c75a1501476375eef66
Brunchcode <anayo.uxui.fullstack@gmail.com>
2022-04-09 02:07:56 -0700

    C - Variables, if, else, while task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char base16;
+
+for (base16 = 48; base16 <=57; base16++)
+{
+putchar(base16);
+}
+
+putchar('\n');
+
+return (0);
+
+}
