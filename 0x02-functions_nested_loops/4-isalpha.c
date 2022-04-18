a856edb51854b6fecfcccdd1ff1d585b06701549
Brunchcode <anayo.uxui.fullstack@gmail.com>
2022-04-18 16:21:53 -0700

    0x02. C - Functions, nested loops 3 and 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ * @c: the int to print
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+if ((c > 'a' && c < 'z') || (c > 'A' && C < 'Z'))
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+}
